a05376eaf737e35d32b0fb1579ac50f5e7d853f0
Can Sönmez <cansonmez06@outlook.com.tr>
2026-02-28 09:57:19 +0100

    feat: community endpoints configured.

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,6 @@ from django.urls import path , include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/users/",include("users.urls")),
-    path("api/v1/",include("workouts.urls")),
+    path("api/v1/workouts/",include("workouts.urls")),
+    path("api/v1/community/",include("community.urls")),
 ]
